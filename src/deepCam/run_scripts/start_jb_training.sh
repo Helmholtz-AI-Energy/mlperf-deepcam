7438fe595166d481fcddbc986d6fd344bc7042ea
coquelin77 <daniel.coquelin@gmail.com>
2021-08-31 11:42:51 +0200

    jb fixes

--- src/deepCam/run_scripts/start_jb_training.sh
+++ src/deepCam/run_scripts/start_jb_training.sh
@@ -7,7 +7,7 @@ ml purge
 
 SRUN_PARAMS=(
   --mpi            pspmix
-  --cpu-bind       none
+#  --cpu-bind       none
   #--label
 )
 
@@ -18,14 +18,19 @@ export DATA_DIR_PREFIX="/p/scratch/jb_benchmark/deepCam/"
 export OUTPUT_ROOT="/p/project/jb_benchmark/MLPerf-1.0/run-logs"
 
 export WIREUP_METHOD="nccl-slurm"
+export SEED="50"
+
+export DEEPCAM_DIR="/opt/deepCam/"
 #export CUDA_AVAILABLE_DEVICES="0,1,2,3"
 
 SCRIPT_DIR="/p/project/jb_benchmark/MLPerf-1.0/mlperf-deepcam/src/deepCam/run_scripts/"
 SINGULARITY_FILE="/p/project/jb_benchmark/MLPerf-1.0/mlperf-deepcam/docker/nvidia-deepcam.sif"
 
+CONFIG_FILE="${SCRIPT_DIR}configs/best_configs/config_DGXA100_128GPU_BS128_graph.sh"
+
 srun "${SRUN_PARAMS[@]}" singularity exec --nv \
   --bind "${DATA_DIR_PREFIX}":/data ${SINGULARITY_FILE} \
     bash -c "\
-      source ${SCRIPT_DIR}configs/base_config.sh; \
-	    export SLURM_CPU_BIND_USER_SET=\"none\"; \
+      source ${CONFIG_FILE}; \
+      export SLURM_CPU_BIND_USER_SET=\"none\"; \
       bash run_and_time.sh"

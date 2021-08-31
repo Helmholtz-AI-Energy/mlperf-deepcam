b7eaa05e8aea1cf37039aa4742b48d7a62ab1bf2
coquelin77 <daniel.coquelin@gmail.com>
2021-08-31 11:24:05 +0200

    added horeka fixes

--- src/deepCam/run_scripts/start_horeka_training.sh
+++ src/deepCam/run_scripts/start_horeka_training.sh
@@ -7,7 +7,7 @@ ml purge
 SRUN_PARAMS=(
   --mpi="pmi2"
   #--cpu-bind="ldoms"
-  --label
+#  --label
 )
 
 export SLURM_CPU_BIND_USER_SET="ldoms"
@@ -15,15 +15,17 @@ export DATA_DIR_PREFIX="/hkfs/home/dataset/datasets/deepcam_npy/"
 export OUTPUT_ROOT="/hkfs/work/workspace/scratch/qv2382-mlperf/mlperf-deepcam/run-logs/"
 
 export WIREUP_METHOD="nccl-slurm-pmi"
+export SEED="50"
 #export CUDA_AVAILABLE_DEVICES="0,1,2,3"
 export DEEPCAM_DIR="/hkfs/work/workspace/scratch/qv2382-mlperf/mlperf-deepcam/image-src/"
 
 SCRIPT_DIR="/hkfs/work/workspace/scratch/qv2382-mlperf/mlperf-deepcam/src/deepCam/run_scripts/"
 SINGULARITY_FILE="/hkfs/work/workspace/scratch/qv2382-mlperf/mlperf-deepcam/docker/nvidia-optimized-torch.sif"
+CONFIG_FILE="${SCRIPT_DIR}configs/best_configs/config_DGXA100_128GPU_BS128_graph.sh"
 
 srun "${SRUN_PARAMS[@]}" singularity exec --nv \
-  --bind "${DATA_DIR_PREFIX}":/data,${SCRIPT_DIR},${OUTPUT_ROOT} ${SINGULARITY_FILE} \
+  --bind "${DATA_DIR_PREFIX}",${SCRIPT_DIR},${OUTPUT_ROOT},${DEEPCAM_DIR} ${SINGULARITY_FILE} \
     bash -c "\
-      source ${SCRIPT_DIR}configs/base_horeka.sh; \
-	    export SLURM_CPU_BIND_USER_SET=\"none\"; \
+      source ${CONFIG_FILE}; \
+      export SLURM_CPU_BIND_USER_SET=\"none\"; \
       bash run_and_time.sh"

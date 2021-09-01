1a214715cdcb91a8b45c511e726186dc301f6667
coquelin77 <daniel.coquelin@gmail.com>
2021-09-01 16:36:48 +0200

    minor updates to jb scripts/512 gpu config

--- src/deepCam/run_scripts/start_jb_training.sh
+++ src/deepCam/run_scripts/start_jb_training.sh
@@ -21,15 +21,16 @@ export WIREUP_METHOD="nccl-slurm"
 export SEED="50"
 
 export DEEPCAM_DIR="/opt/deepCam/"
+#export DEEPCAM_DIR="/p/project/jb_benchmark/MLPerf-1.0/mlperf-deepcam/image-src/"
 #export CUDA_AVAILABLE_DEVICES="0,1,2,3"
 
 SCRIPT_DIR="/p/project/jb_benchmark/MLPerf-1.0/mlperf-deepcam/src/deepCam/run_scripts/"
 SINGULARITY_FILE="/p/project/jb_benchmark/MLPerf-1.0/mlperf-deepcam/docker/nvidia-deepcam.sif"
 
-CONFIG_FILE="${SCRIPT_DIR}configs/best_configs/config_DGXA100_128GPU_BS128_graph.sh"
+CONFIG_FILE="${SCRIPT_DIR}configs/best_configs/config_DGXA100_512GPU_BS1024_graph.sh"
 
 srun "${SRUN_PARAMS[@]}" singularity exec --nv \
-  --bind "${DATA_DIR_PREFIX}":/data ${SINGULARITY_FILE} \
+  --bind "${DATA_DIR_PREFIX}",${SCRIPT_DIR},${OUTPUT_ROOT} ${SINGULARITY_FILE} \
     bash -c "\
       source ${CONFIG_FILE}; \
       export SLURM_CPU_BIND_USER_SET=\"none\"; \
